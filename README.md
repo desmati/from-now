42da2cbf96ef53b480f87d4d1cf504e43bfead20
desmati <desmati@live.com>
2022-11-07 11:42:48 +0100

    sharing with others

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trying to create a function that calculates how much time is passed from given date object.
+
+## Sample Output:
+
+10 second(s) ago
+8 minute(s) ago
+3 hour(s) ago
+1 day(s) ago
+2 week(s) ago
+4 month(s) ago
+1 year(s) ago
---
+++ script.js
@@ -0,0 +1,49 @@
+let aMinute = 60;
+let anHour = aMinute * 60;
+let aDay = anHour * 24;
+let aWeek = aDay * 7;
+let aMonth = aDay * 30;
+let aYear = aMonth * 12;
+
+function fromNow(date) {
+    const now = new Date()    // return date;
+    let seconds = (now - date) / 1000;
+
+    let result = seconds;
+    let unit = 'second(s)'
+
+
+    if (seconds >= aMinute && seconds < anHour) {
+        result = seconds / aMinute;
+        unit = 'minute(s)';
+    } else if (seconds >= anHour && seconds < aDay) {
+        result = seconds / anHour;
+        unit = 'hour(s)';
+    } else if (seconds >= aDay && seconds < aWeek) {
+        result = seconds / aDay;
+        unit = 'day(s)';
+    } else if (seconds >= aWeek && seconds < aMonth) {
+        result = seconds / aWeek;
+        unit = 'week(s)';
+    } else if (seconds >= aMonth && seconds < aYear) {
+        result = seconds / aMonth;
+        unit = 'month(s)';
+    } else if (seconds >= aYear) {
+        result = seconds / aYear;
+        unit = 'year(s)';
+    }
+
+    result = Math.floor(result);
+
+    return `${result} ${unit} ago`;
+}
+
+let output = document.getElementById('output');
+
+output.innerHTML += fromNow(new Date(Date.now() - 10 * 1000)) + '<br/><br/>';
+output.innerHTML += fromNow(new Date(Date.now() - aMinute * 8000)) + '<br/><br/>';
+output.innerHTML += fromNow(new Date(Date.now() - anHour * 3000)) + '<br/><br/>';
+output.innerHTML += fromNow(new Date(Date.now() - aDay * 1000)) + '<br/><br/>';
+output.innerHTML += fromNow(new Date(Date.now() - aWeek * 2000)) + '<br/><br/>';
+output.innerHTML += fromNow(new Date(Date.now() - aMonth * 4000)) + '<br/><br/>';
+output.innerHTML += fromNow(new Date(Date.now() - aYear * 1000)) + '<br/><br/>';
